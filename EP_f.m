631e5505a19f114e7ace5ef158d2fac9442e9c5b
BigChaoor <114349415+BigChaoor@users.noreply.github.com>
2023-05-27 10:24:25 +0800

    Delete EP_f.m

--- EP_f.m
+++
@@ -1,4 +0,0 @@
-function F = EP_f(y,x,stepsize)
-BGH = [-0.25*(x(1) + y(1))*(x(2) + y(2))*(-1 - 0.5*(log(abs(y(2))) - log(abs(x(2))))/(y(2) - x(2)))
-0.25*(x(1) + y(1))*(x(2) + y(2))*(0.4 + (log(abs(y(1))) - log(abs(x(1))))/(y(1) - x(1)))];
-F =x + BGH*stepsize;

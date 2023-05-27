6405fc63bd568530bc5c42b30013f9abc35106a6
BigChaoor <114349415+BigChaoor@users.noreply.github.com>
2023-05-27 10:12:48 +0800

    Update and rename EP_phi.m to EnergyPreserving_phi.m

--- EnergyPreserving_phi.m
+++ EnergyPreserving_phi.m
@@ -1,6 +1,5 @@
 function F = EP_phi(y,x,stepsize)
-% x = y0;
-% y = y0+h;
+
 global a b d gamma mu
 BGH = [(d*(x(1) + y(1))*(x(2) + y(2))*((gamma*(log(abs((x(2)))) - log(abs((y(2))))))/(x(2) - y(2)) + 1))/4 - (b*d*(a + (mu*(log(abs((x(3)))) - log(abs((y(3))))))/(x(3) - y(3)))*(x(1) + y(1))*(x(3) + y(3)))/4
     ((a + (mu*(log(abs((x(3)))) - log(abs((y(3))))))/(x(3) - y(3)))*(x(2) + y(2))*(x(3) + y(3)))/4 - (a*b*d*(x(1) + y(1))*(x(2) + y(2)))/4
